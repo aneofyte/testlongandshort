31010b73730d3c9bcddc9ba671203615cf57701a
Lance Rushing <lancerushing@gmail.com>
2010-09-16 23:58:35 -0700

    Changed me to lance

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from google.appengine.ext.webapp import util
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello me!')
+        self.response.out.write('Hello lance!')
 
 
 def main():
